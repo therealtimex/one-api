e42e3fbabb53fd519e8087c1ee36aed6f87c4360
Phuong Nguyen <phuongnguyen@rtanalytics.vn>
2025-03-25 08:20:53 +0700

    update channel fetching

--- middleware/distributor.go
+++ middleware/distributor.go
@@ -26,35 +26,45 @@ func Distribute() func(c *gin.Context) {
 		var requestModel string
 		var channel *model.Channel
 		channelId, ok := c.Get(ctxkey.SpecificChannelId)
+
+		hasChannel = true
 		if ok {
 			id, err := strconv.Atoi(channelId.(string))
 			if err != nil {
-				abortWithMessage(c, http.StatusBadRequest, "无效的渠道 Id")
-				return
+				// abortWithMessage(c, http.StatusBadRequest, "无效的渠道 Id")
+				// return
+				hasChannel = false
 			}
 			channel, err = model.GetChannelById(id, true)
 			if err != nil {
-				abortWithMessage(c, http.StatusBadRequest, "无效的渠道 Id")
-				return
+				// abortWithMessage(c, http.StatusBadRequest, "无效的渠道 Id")
+				// return
+				hasChannel = false
 			}
 			if channel.Status != model.ChannelStatusEnabled {
-				abortWithMessage(c, http.StatusForbidden, "该渠道已被禁用")
-				return
+				// abortWithMessage(c, http.StatusForbidden, "该渠道已被禁用")
+				// return
+				hasChannel = false
 			}
-		} else {
-			requestModel = c.GetString(ctxkey.RequestModel)
-			var err error
+		} 
+		
+		requestModel = c.GetString(ctxkey.RequestModel)
+		
+		var err error
+		if !hasChannel {
 			channel, err = model.CacheGetRandomSatisfiedChannel(userGroup, requestModel, false)
-			if err != nil {
-				message := fmt.Sprintf("当前分组 %s 下对于模型 %s 无可用渠道", userGroup, requestModel)
-				if channel != nil {
-					logger.SysError(fmt.Sprintf("渠道不存在：%d", channel.Id))
-					message = "数据库一致性已被破坏，请联系管理员"
-				}
-				abortWithMessage(c, http.StatusServiceUnavailable, message)
-				return
-			}
 		}
+		
+		if err != nil {
+			message := fmt.Sprintf("当前分组 %s 下对于模型 %s 无可用渠道", userGroup, requestModel)
+			if channel != nil {
+				logger.SysError(fmt.Sprintf("渠道不存在：%d", channel.Id))
+				message = "数据库一致性已被破坏，请联系管理员"
+			}
+			abortWithMessage(c, http.StatusServiceUnavailable, message)
+			return
+		}
+		
 		logger.Debugf(ctx, "user id %d, user group: %s, request model: %s, using channel #%d", userId, userGroup, requestModel, channel.Id)
 		SetupContextForSelectedChannel(c, channel, requestModel)
 		c.Next()
